269a2f50a02861416c0879938de3e9b162fb1114
hbuurmei <hugo.buurmeijer@gmail.com>
2024-10-22 13:32:32 -0700

    Beta rnd data collection

--- stack/main/scripts/control_inputs_sampling.py
+++ stack/main/scripts/control_inputs_sampling.py
@@ -42,35 +42,11 @@ def uniform_sampling(control_variables):
         control_inputs_df = control_inputs_df._append(dict(zip(['ID'] + control_variables, [i] + list(combo))), ignore_index=True)
 
     return control_inputs_df
-
-
-def random_sampling(control_variables):
-    sample_size = 250
-    np.random.seed(9)
-    tip_range = 0.15
-    mid_range = 0.25
-    base_range = 0.35 
-
-    control_inputs_df = pd.DataFrame(columns=['ID'] + control_variables)
-    
-    u1 = np.random.uniform(low=-tip_range, high=tip_range, size=(sample_size,))
-    u6 = np.random.uniform(low=-tip_range, high=tip_range, size=(sample_size,))
-    u2 = np.random.uniform(low=-mid_range, high=mid_range, size=(sample_size,))
-    u5 = np.random.uniform(low=-mid_range, high=mid_range, size=(sample_size,))
-    u3 = np.random.uniform(low=-base_range, high=base_range, size=(sample_size,))
-    u4 = np.random.uniform(low=-base_range, high=base_range, size=(sample_size,))
-
-    data = np.column_stack([u1, u2, u3, u4, u5, u6])
-    ids = np.arange(0, sample_size)
-    control_inputs_df = pd.DataFrame(data, columns=control_variables)
-    control_inputs_df.insert(0, 'ID', ids)
-
-    return control_inputs_df
     
 
 def beta_sampling(control_variables, sample_size=150):
     np.random.seed(0)
-    tip_range, mid_range, base_range = 0.15, 0.25, 0.35 
+    tip_range, mid_range, base_range = 0.2, 0.3, 0.4 
 
     control_inputs_df = pd.DataFrame(columns=['ID'] + control_variables)
     
@@ -137,8 +113,6 @@ def main(data_type='dynamic', sampling_type='uniform'):
         control_inputs_df = uniform_sampling(control_variables)
     elif sampling_type=='beta':
         control_inputs_df = beta_sampling(control_variables)
-    elif sampling_type=='random':
-        control_inputs_df = random_sampling(control_variables)
     else:
         raise ValueError(f"Invalid sampling_type: {sampling_type}")
 
@@ -148,5 +122,5 @@ def main(data_type='dynamic', sampling_type='uniform'):
 
 if __name__ == '__main__':
     data_type = 'steady_state'       # 'steady_state' or 'dynamic'
-    sampling_type = 'beta'           # 'beta', 'uniform', 'sinusoidal' or 'random'
+    sampling_type = 'beta'           # 'beta', 'uniform' or 'sinusoidal'
     main(data_type, sampling_type)
