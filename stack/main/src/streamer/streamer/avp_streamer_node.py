2d65918b4a334a21051750598ddd0aec0fcb408c
hbuurmei <hugo.buurmeijer@gmail.com>
2024-10-28 17:52:44 -0700

    added knn interpolation controller. sampled targeted inputs seed 0-4

--- stack/main/src/streamer/streamer/avp_streamer_node.py
+++ stack/main/src/streamer/streamer/avp_streamer_node.py
@@ -62,7 +62,7 @@ class AVPStreamerNode(Node):
                     QoSProfile(depth=10)
                 )
 
-        self.streamer = AVPSubscriber(ip='10.93.181.122')
+        self.streamer = AVPSubscriber(ip='10.93.181.166') # SET IP ADDRESS HERE
         self._timer = self.create_timer(1.0 / 10.0, self.streamer_data_sampling_callback) # runs at 10Hz
         self.last_isGripperOpen = self.streamer.isGripperOpen #should start as closed
         
