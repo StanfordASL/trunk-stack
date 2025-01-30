44963b14ee0fc48bd3fa300919d80c7f2de7d873
hbuurmei <hugo.buurmeijer@gmail.com>
2025-01-29 21:34:37 -0800

    Working model conversion from matlab

--- stack/main/scripts/convert_mat_to_npz.py
+++ stack/main/scripts/convert_mat_to_npz.py
@@ -3,16 +3,13 @@ import scipy.io
 import numpy as np
 
 
-def mat_to_npz(mat_filepath, npz_filepath=None):
+def mat_to_npz(mat_filepath, npz_filepath):
     """
     Loads a.mat file and saves its contents as a.npz file.
 
     Args:
         mat_filepath (str): The path to the.mat file.
-        npz_filepath (str, optional): The path to save the.npz file. 
-                                      If None, the npz file will be saved in the same 
-                                      directory as the mat file with the same name 
-                                      but the.npz extension. Defaults to None.
+        npz_filepath (str): The path to save the.npz file. 
 
     Returns:
         str: The path to the saved.npz file.
@@ -28,10 +25,6 @@ def mat_to_npz(mat_filepath, npz_filepath=None):
         print(f"Error loading.mat file: {e}")
         return None
 
-    if npz_filepath is None:
-        base_name = os.path.splitext(os.path.basename(mat_filepath))
-        npz_filepath = os.path.join(os.path.dirname(mat_filepath), base_name + ".npz")
-
     try:
         np.savez(npz_filepath, **mat_data)  # Save as.npz, unpacking the dictionary
         print(f"Successfully saved as.npz at {npz_filepath}")
@@ -41,11 +34,14 @@ def mat_to_npz(mat_filepath, npz_filepath=None):
         return None
 
 
-def main(args=None):
-    model_name = "origin_ssmr_200g"
+def main():
+    model_name = 'origin_ssmr_200g'
     data_dir = os.getenv('TRUNK_DATA', '/home/trunk/Documents/trunk-stack/stack/main/data')
     mat_filepath = os.path.join(data_dir, f'models/ssmr/{model_name}.mat')
-    saved_path = mat_to_npz(mat_filepath)
+    npz_filepath = os.path.join(data_dir, f'models/ssmr/{model_name}.npz')
+    saved_path = mat_to_npz(mat_filepath, npz_filepath)
+    data = np.load(saved_path)
+    print('Keys: ', list(data.keys()))
 
 if __name__ == '__main__':
     main()
