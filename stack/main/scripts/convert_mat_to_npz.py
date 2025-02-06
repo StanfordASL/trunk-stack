0f5e798fb16d5a066e7d4fce3bfa95f9e5833ea2
hbuurmei <hugo.buurmeijer@gmail.com>
2025-02-06 13:34:36 -0800

    l

--- stack/main/scripts/convert_mat_to_npz.py
+++ stack/main/scripts/convert_mat_to_npz.py
@@ -3,6 +3,32 @@ import scipy.io
 import numpy as np
 
 
+def mat_to_npz_slow(mat_filepath, npz_filepath):
+    """
+    Loads a.mat file and saves its contents as a.npz file.
+
+    Args:
+        mat_filepath (str): The path to the.mat file.
+        npz_filepath (str): The path to save the.npz file. 
+
+    Returns:
+        str: The path to the saved.npz file.
+        None: If there is an error during the process.
+    """
+
+
+    mat_data = scipy.io.loadmat(mat_filepath)
+    decoder_coeff, decoder_exp = mat_data['Slow_manifold_coeff'], mat_data['exps_sm']
+    Const_coeff = mat_data['Const_coeff']
+    np.savez(
+        npz_filepath,
+        decoder_coeff=decoder_coeff,
+        decoder_exp=decoder_exp,
+        Const_coeff=Const_coeff,
+    )
+    print(f"Successfully saved as.npz at {npz_filepath}")
+    return npz_filepath
+
 def mat_to_npz(mat_filepath, npz_filepath):
     """
     Loads a.mat file and saves its contents as a.npz file.
@@ -40,14 +66,14 @@ def mat_to_npz(mat_filepath, npz_filepath):
 
 
 def main():
-    model_name = 'ssm_origin_300g_fast'
+    model_name = 'slow_manifold_dataset'
     data_dir = os.getenv('TRUNK_DATA', '/home/trunk/Documents/trunk-stack/stack/main/data')
     mat_filepath = os.path.join(data_dir, f'models/ssm/{model_name}.mat')
     npz_filepath = os.path.join(data_dir, f'models/ssm/{model_name}.npz')
-    saved_path = mat_to_npz(mat_filepath, npz_filepath)
+    saved_path = mat_to_npz_slow(mat_filepath, npz_filepath)
     data = np.load(saved_path)
-    print('Keys: ', list(data.keys()))
-    print('B_r matrix: ', data['B_r_coeff'])
+    # print('Keys: ', list(data.keys()))
+    # print('B_r matrix: ', data['B_r_coeff'])
 
 if __name__ == '__main__':
     main()
