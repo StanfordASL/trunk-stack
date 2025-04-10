4f41e28b4431ca717d9e1c9cc6d95c988f6d2f15
Trunk Robot <trunk@rpi.stanford.edu>
2025-04-10 03:25:30 +0000

    new motors workspace working

---
+++ stack/motors/src/trunk_motors/trunk_motors/launch/launch_motors.py
@@ -0,0 +1,22 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='trunk_motors',
+            executable='motor_node.py',
+            name='motor_node',
+            emulate_tty=True,
+            output='screen',
+            parameters=[
+                {'kP': 800.0},
+                {'kI': 0.0},
+                {'kD': 0.0},
+                {'curr_lim': 3000.0}
+            ]
+        )
+    ])
