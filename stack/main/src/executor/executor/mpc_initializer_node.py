6c750bb1112e388cea1baefb33cdee3958cecf0f
asltrunk <asltrunk@gmail.com>
2025-06-20 23:22:14 -0700

    include 3 cm eights

--- stack/main/src/executor/executor/mpc_initializer_node.py
+++ stack/main/src/executor/executor/mpc_initializer_node.py
@@ -33,7 +33,7 @@ class MPCInitializerNode(Node):
         config = {
             "mpc": {
                 "Q_rows": [0, 1],
-                "Qz": 700.0, # was circles: 500, 1000
+                "Qz": 700.0, # was circles: 500, 1000, eight: 700
                 "Qzf": 2000.0,  # was 640
                 "R": 0.0,
                 "Rdu": 16.0,
@@ -45,12 +45,12 @@ class MPCInitializerNode(Node):
             "trajectory": {
                 "type": "eight",  # Options: "circle", "circle_with_ramp", "eight", "pacman", "pacman_with_ramp", "flower"
                 "duration": 20.0,  # Duration of the simulation in seconds
-                "speed": 1.0,  # Angular speed (rad/s)
+                "speed": 1.5,  # Angular speed (rad/s)
                 "include_velocity": False,
                 "parameters": {
                     "center": [0.0, 0.0],  # Center of the (x,y) trajectory
                     "radius": 0.05,  # [m]  For "circle" and "pacman"
-                    "amplitude": 0.05,  # [m]  For "eight"
+                    "amplitude": 0.03,  # [m]  For "eight"
                     "z_level": 0.0,  # [m]  Constant z-coordinate
                     "mouth_angle": 0.7854  # [rad] Defines the size of the pacman mouth (default π/4)
                 }
