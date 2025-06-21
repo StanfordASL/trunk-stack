a2258a9ade1dce07476b0b42f2fe451d737b5e0d
asltrunk <asltrunk@gmail.com>
2025-06-20 17:47:42 -0700

    add run

--- stack/main/src/executor/executor/mpc_initializer_node.py
+++ stack/main/src/executor/executor/mpc_initializer_node.py
@@ -33,7 +33,7 @@ class MPCInitializerNode(Node):
         config = {
             "mpc": {
                 "Q_rows": [0, 1],
-                "Qz": 1000.0,
+                "Qz": 2000.0,
                 "Qzf": 2000.0,  # was 640
                 "R": 0.0,
                 "Rdu": 16.0,
