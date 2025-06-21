d83518bdd9850c4d519c7b78508c6bf32a33b313
Paul <wolffp@student.ethz.ch>
2025-06-21 15:36:15 -0700

    Import motors/src from control-augmented-stack

--- stack/motors/src/trunk_motors/trunk_motors/launch/launch_motors.py
+++ stack/motors/src/trunk_motors/trunk_motors/launch/launch_motors.py
@@ -2,10 +2,18 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Declare launch argument with default 'true'
+        DeclareLaunchArgument(
+            'secure_mode',
+            default_value='true',
+            description='Enable or disable secure mode for motor_node'
+        ),
         Node(
             package='trunk_motors',
             executable='motor_node.py',
@@ -16,7 +24,8 @@ def generate_launch_description():
                 {'kP': 800.0},
                 {'kI': 0.0},
                 {'kD': 0.0},
-                {'curr_lim': 3000.0}
+                {'curr_lim': 3000.0},
+                {'secure_mode': LaunchConfiguration('secure_mode')}
             ]
         )
     ])
