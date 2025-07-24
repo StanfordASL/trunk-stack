03e582fd9fa63cd6ace24842667c05623e99a32b
asltrunk <asltrunk@gmail.com>
2025-07-23 17:52:51 -0700

    Working autonomous ssm model baseline

--- stack/main/src/executor/executor/utils/ssm.py
+++ stack/main/src/executor/executor/utils/ssm.py
@@ -11,7 +11,7 @@ from sklearn.utils.extmath import randomized_svd
 from scipy.linalg import orth
 import numpy as np
 import sympy as sp
-from .misc import trajectories_delay_embedding
+from .misc import trajectories_delay_embedding, trajectories_derivatives
 
 
 class DelaySSM:
@@ -279,7 +279,40 @@ class DelaySSM:
         return equations
 
 
-def generate_ssm_predictions(delay_ssm: DelaySSM, trajs, ts=None, dt=None):
+def get_residual_labels(ssm, trajs, ts, u_func=None, rnd_key=jax.random.PRNGKey(0), us=None):
+    """
+    Get labels for B_r learning.
+    """
+    # Either provide the control inputs or the control function
+    if us is None and u_func is None:
+        raise ValueError("Either control inputs or control function must be provided.")
+
+    N_trajs = len(trajs)
+    ys = trajectories_delay_embedding(trajs, ssm.N_obs_delay)
+    x_trajs = []
+    for traj in ys:
+        x_traj = ssm.encode(traj)
+        # Apply padding of zeros to the end of the trajectory
+        x_traj = x_traj.at[:, -(ssm.N_obs_delay):].set(jnp.zeros((ssm.SSMDim, ssm.N_obs_delay)))
+        x_trajs.append(x_traj)
+    x_trajs = jnp.array(x_trajs)
+
+    x_dots_ctrl = trajectories_derivatives(x_trajs, ts)
+    x_dots_aut = []
+    for traj in x_trajs:
+        x_dot_aut = ssm.reduced_dynamics(traj)
+        x_dots_aut.append(x_dot_aut)
+    x_dots_aut = jnp.array(x_dots_aut)
+
+    delta_x_dots = x_dots_ctrl - x_dots_aut
+    delta_x_dots_flat = delta_x_dots.transpose(0, 2, 1).reshape(-1, ssm.SSMDim)
+    xs_flat = x_trajs.transpose(0, 2, 1).reshape(-1, ssm.SSMDim)
+    us = u_func(ts, N_trajs, rnd_key) if us is None else us  # shape of us is (N_trajs, n_u, len(ts))
+    us_flat = us.transpose(0, 2, 1).reshape(-1, us.shape[1])
+    return xs_flat, us_flat, delta_x_dots_flat
+
+
+def generate_ssm_predictions(ssm, trajs, ts=None, dt=None):
     """
     Generate tip positions as predicted by SSM model.
     """
@@ -288,13 +321,14 @@ def generate_ssm_predictions(delay_ssm: DelaySSM, trajs, ts=None, dt=None):
         ts = np.arange(0, steps * dt, dt)
 
     N_input_states = trajs.shape[1]
+    N_obs_delay = ssm.N_obs_delay
     ssm_predictions = jnp.zeros_like(trajs)
     for i, traj in enumerate(trajs):
-        # Assume first 2 observations are known
-        ssm_predictions = ssm_predictions.at[i, :, :2].set(traj[:, :2])
-        y0 = jnp.flip(traj[:, :2], 1).T.flatten()
-        x0 = delay_ssm.encode(y0)
-        xs = delay_ssm.simulate_reduced(x0, ts[2:])
-        ys = delay_ssm.decode(xs)
-        ssm_predictions = ssm_predictions.at[i, :, 2:].set(ys[:N_input_states, :])
+        # Assume first (N_obs_delay + 1) observations are known
+        ssm_predictions = ssm_predictions.at[i, :, :N_obs_delay+1].set(traj[:, :N_obs_delay+1])
+        y0 = jnp.flip(traj[:, :N_obs_delay+1], 1).T.flatten()
+        x0 = ssm.encode(y0)
+        xs = ssm.simulate_reduced(x0, ts[N_obs_delay+1:])
+        ys = ssm.decode(xs)
+        ssm_predictions = ssm_predictions.at[i, :, N_obs_delay+1:].set(ys[:N_input_states, :])
     return ssm_predictions
